2a0bc0bed30bc582f604352c41b073fe53b08ce5
Raphael MV <rap_mv@yahoo.com.br>
2026-04-15 14:03:40 -0300

    Final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.dominio.Bootcamp;
 import org.example.dominio.Conteudo;
 import org.example.dominio.Curso;
+import org.example.dominio.Dev;
 import org.example.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -36,6 +37,35 @@ public class Main {
         System.out.println(mentoria);
          */
 
-        
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAluno1 = new Dev();
+        devAluno1.setNome("Aluno1");
+        devAluno1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Aluno1: " + devAluno1.getConteudosInscritos());
+        devAluno1.progredir();
+        System.out.println("Conteúdos Concluidos Aluno1: " + devAluno1.getConteudosConcluidos());
+        System.out.println("XP: " + devAluno1.calcularTotalXp());
+        System.out.println("----------------------------------------------------------------");
+
+        Dev devAluno2 = new Dev();
+        devAluno2.setNome("Aluno2");
+        devAluno2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Aluno2: " + devAluno2.getConteudosInscritos());
+        devAluno2.progredir();
+        devAluno2.progredir();
+        System.out.println("Conteúdos Concluidos Aluno2: " + devAluno2.getConteudosConcluidos());
+        System.out.println("XP: " + devAluno2.calcularTotalXp());
+        System.out.println("----------------------------------------------------------------");
+
+        System.out.println("Conteúdos Inscritos Aluno1: " + devAluno1.getConteudosInscritos());
+        System.out.println("Conteúdos Inscritos Aluno2: " + devAluno2.getConteudosInscritos());
+
+
     }
 }
